bf19af11a36850730711005d430652ef190d1223
NihachiOokami <paulsode05@gmail.com>
2024-03-16 11:42:29 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # MUSQLAC-ChatBot
+
+## Exécuter le projet
+Téléchargez les fichiers "index.html", "index.js", et "style.css" dans un même dossier.
+Ouvrez index.js.
+
+## Processus de prompt engineering
+Utilisation de "Instruction-based prompts" et "Contextual prompts"
+
+Tentative d'utilisation de prompts simple tells que 
+"Voici les symptômes du patient : {Symptomes}. Quel traitement devrait-il recevoir"
+
+Puis complexification des demandes, ajout de contexte, ajout de dictionnaire, ajout de questions supplémentaires, nettoyage des réponses
+
+
+## Scénarios
+### Scénario 1
+Utilisateur : I have chest pain
+Bot : You might have pneumonia. I need more informations to be sure. Do you have a cough ? Do you have a fever ? Do you have a history of smoking ? If you have the answer to one of these questions, please answer.
+Utilisateur : I have a little cough, a little fever, and i do not smoke
+Bot : You might have a simple cough, or a pneumonia. You should drink a lot of water, and if the cough persists, you should see a doctor.
+
+
+### Scénario 2  
+Utilisateur : I have pain in my leg
+Bot : It could be a muscle pain, or a bone pain. Can you describe your pain ? - You haven't told me about your pain's location. Where do you feel the pain ? - Your pain is not very clear. Can you describe it ? Is it a dull, a sharp or a burning pain ? - You haven't told me about the pain's strength. How strong is the pain ?
+Utilisateur : Acutepain, near the knee, sharp
+Bot : You might have a meniscal tear, you should probagly go to the doctor.
--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const chatForm = get('form');
 const chatInput = get('input');
 const chatBox = get('main');
+const botFirstMessage =
+  "Hello, I am Chat-CPT, a chatbot designed to help you choose a treatment depending on your symptoms. Please describe your symptoms.";
+let botLastMessage = botFirstMessage;
+let userLastMessage = "";
+const ctx =  `
+You're an AI ChatBOT, specialized detecting medical problems based on symptoms.
+You are going to talk with a user, and your goal is to identify health problems based on its symptoms. 
 
-appendMessage('bot', 'Hello, I am Chat-CPT, a chatbot designed to help you choose a treatment depending on your symptoms. Please describe your symptoms.')
+Your answer will be based on your discussion progress, the informations you've already harvested and the missing informations.
+Ask as many questions as needed to the user.
+
+Here are some questions you can ask before proceeding to the diagnostic:
+- Can you describe your symptoms ?
+- Where do you feel the pain ?
+- How strong is the pain ?
+- What type of pain do you feel ?
+- Do you have a high fever ?
+- How many days have passed since the first symptoms ?
+- Do you have any other symptoms ?`
+
+appendMessage("bot", botFirstMessage);
 
 chatForm.addEventListener('submit', event => {
   event.preventDefault();
@@ -13,12 +32,18 @@ chatForm.addEventListener('submit', event => {
   tempMessage()
   chatInput.value = '';
 
+  let finalText = prepareText(text);
+
   query({
-    "inputs": text,
-    "parameters": {}
-}).then((response) => {
-  botAnswerReceived(response[0].generated_text)
-});
+    inputs: finalText,
+    parameters: {},
+  }).then((response) => {
+    console.log("INITIAL : ", response[0].generated_text);
+    res = prettyAnswer(response[0].generated_text);
+    botLastMessage = res;
+    userLastMessage = finalText;
+    botAnswerReceived(res);
+  });
 });
 
 function tempMessage(){
@@ -70,4 +95,32 @@ async function query(data) {
 	);
 	const result = await response.json();
 	return result;
+}
+
+function prettyAnswer(answer) {
+  console.log("answer :", answer);
+  if (
+    answer.includes("Your answer is :") ||
+    (answer.includes(":") && answer.split(":")[0].includes("You"))
+  ) {
+    let a = answer.split(":")[1];
+    if (a.includes("?")) {
+      return a.split("?")[0];
+    }
+  }
+  return answer;
+}
+
+function prepareText(text) {
+  return `
+    Context : ${ctx}\n
+
+    You will ask questions for each missing informations, ont at a time, and will wait for the user to answer.
+    Do not invent responses, only ask questions based on the informations you already have.\n
+    If you have enough informations to make a diagnostic, you can make a diagnostic and tell the user about it.
+    If not, ask a new question to the user to get the missing informations, and wait fot the user's answer.
+
+    The user's message is : ${text}
+    Your answer is :
+  `;
 }
